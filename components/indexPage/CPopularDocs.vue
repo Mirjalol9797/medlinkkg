26c41698135fb46a821d71b3f4e19a224712fc00
Mirjalol <miramirxomitov97@gmail.com>
2024-11-23 16:15:24 +0500

    added all info for popular docs

--- components/indexPage/CPopularDocs.vue
+++ components/indexPage/CPopularDocs.vue
@@ -23,9 +23,9 @@
           },
         }"
       >
-        <SwiperSlide v-for="(item, index) in 7" :key="index">
+        <SwiperSlide>
           <nuxt-link :to="localePath(`/doctors/terapevt`)" class="relative">
-            <div class="">
+            <div class="h-[140px]">
               <img
                 src="@/public/icons/mainPage/popularDocs/terapevt.jpg"
                 alt="terapevt"
@@ -39,6 +39,101 @@
               class="font-medium absolute bg-[#6486fa] z-10 bottom-0 w-full text-center text-white py-2 text-base"
             >
               Терапевт
+            </div>
+          </nuxt-link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <nuxt-link :to="localePath(`/doctors/kardiolog`)" class="relative">
+            <div class="h-[140px]">
+              <img
+                src="@/public/icons/mainPage/popularDocs/kardiolog.jpg"
+                alt="kardiolog"
+                title="kardiolog"
+                class="w-full h-full object-cover"
+                loading="lazy"
+                format="webp"
+              />
+            </div>
+            <div
+              class="font-medium absolute bg-[#ff597f] z-10 bottom-0 w-full text-center text-white py-2 text-base"
+            >
+              Кардиолог
+            </div>
+          </nuxt-link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <nuxt-link :to="localePath(`/doctors/pediatr`)" class="relative">
+            <div class="h-[140px]">
+              <img
+                src="@/public/icons/mainPage/popularDocs/pediatr.jpg"
+                alt="pediatr"
+                title="pediatr"
+                class="w-full h-full object-cover"
+                loading="lazy"
+                format="webp"
+              />
+            </div>
+            <div
+              class="font-medium absolute bg-[#4c6ddf] z-10 bottom-0 w-full text-center text-white py-2 text-base"
+            >
+              Педиатр
+            </div>
+          </nuxt-link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <nuxt-link :to="localePath(`/doctors/lor`)" class="relative">
+            <div class="h-[140px]">
+              <img
+                src="@/public/icons/mainPage/popularDocs/lor.jpg"
+                alt="lor"
+                title="lor"
+                class="w-full h-full object-cover"
+                loading="lazy"
+                format="webp"
+              />
+            </div>
+            <div
+              class="font-medium absolute bg-[#ec8d4e] z-10 bottom-0 w-full text-center text-white py-2 text-base"
+            >
+              ЛОР
+            </div>
+          </nuxt-link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <nuxt-link :to="localePath(`/doctors/nevrolog`)" class="relative">
+            <div class="h-[140px]">
+              <img
+                src="@/public/icons/mainPage/popularDocs/nevrolog.jpg"
+                alt="nevrolog"
+                title="nevrolog"
+                class="w-full h-full object-cover"
+                loading="lazy"
+                format="webp"
+              />
+            </div>
+            <div
+              class="font-medium absolute bg-[#fb6285] z-10 bottom-0 w-full text-center text-white py-2 text-base"
+            >
+              Невролог
+            </div>
+          </nuxt-link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <nuxt-link :to="localePath(`/doctors/okulist`)" class="relative">
+            <div class="h-[140px]">
+              <img
+                src="@/public/icons/mainPage/popularDocs/okulist.jpg"
+                alt="okulist"
+                title="okulist"
+                class="w-full h-full object-cover"
+                loading="lazy"
+                format="webp"
+              />
+            </div>
+            <div
+              class="font-medium absolute bg-[#6e8efb] z-10 bottom-0 w-full text-center text-white py-2 text-base"
+            >
+              Офтальмолог
             </div>
           </nuxt-link>
         </SwiperSlide>
