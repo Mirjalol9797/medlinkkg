916f04159054e273eb0d33cd3965d7855e8ea616
Mirjalol <miramirxomitov97@gmail.com>
2024-11-25 13:49:15 +0500

    change seo text

--- app.vue
+++ app.vue
@@ -9,9 +9,9 @@
       content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"
     />
     <Meta name="og:type" property="og:type" content="website" />
-    <Meta name="og:site_name" property="og:site_name" content="TopRankings" />
+    <Meta name="og:site_name" property="og:site_name" content="medlink" />
     <Meta property="twitter:card" content="summary_large_image" />
-    <Meta name="copyright" content="copyright © 2024, toprankings.uz" />
+    <Meta name="copyright" content="copyright © 2024, medlink.kg/" />
     <Meta name="google-adsense-account" content="ca-pub-3990212606361557" />
     <Meta
       name="google-site-verification"
