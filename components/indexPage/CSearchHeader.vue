41c36d63a27270b23f72d97b062d7404d4688303
Mirjalol <miramirxomitov97@gmail.com>
2024-11-25 12:59:24 +0500

    added style search

--- components/indexPage/CSearchHeader.vue
+++ components/indexPage/CSearchHeader.vue
@@ -3,6 +3,20 @@ import vSelect from "vue-select";
 import "vue-select/dist/vue-select.css";
 </script>
 <template>
-  <div class="py-10">Search page</div>
+  <div class="py-14 flex-center gap-5 max-w-[70%] mx-auto">
+    <UiHInput
+      label="Врач, клиника, услуга, препараты"
+      class="border border-[#6486fa] rounded-lg font-semibold"
+    />
+    <UiHInput
+      label="Район, округ, город, улица"
+      class="border border-[#6486fa] rounded-lg font-semibold"
+    />
+    <button
+      class="py-3 px-7 text-white font-semibold text-lg bg-[#6486fa] rounded-lg"
+    >
+      Найти
+    </button>
+  </div>
 </template>
 <style lang="scss" scoped></style>
